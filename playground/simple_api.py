b41921befb014781367b618abe424c56e83779eb
Norbi Bereczki <bnorbertcristian@gmail.com>
2019-11-15 23:20:24 +0200

    Added pipenv and some dummy code

---
+++ playground/simple_api.py
@@ -0,0 +1,7 @@
+import wikipedia as W
+
+#print(W.page("Python").title)
+#print(W.page("Python").url)
+
+#print(W.page("Python").links)
+print(W.page("Guido van Rossum").html())
---
+++ playground/simple_api2.py
@@ -0,0 +1,4 @@
+import wikipedia as W
+
+print(W.page("SageMath").title)
+print(W.page("SageMath").url)
